c8dbc79e466fdb4a041eae4324fca57ae7add475
Alexander Weinstein <alexanderweinstein@DCP-APPLE-2153.local>
2021-11-03 17:03:33 -0400

    add correct reference for applying weights. more references can be found in github issue

--- aggregate/aggregate_PUMS.py
+++ aggregate/aggregate_PUMS.py
@@ -1,6 +1,6 @@
 """First shot at aggregating by PUMA with replicate weights. This process will go
 through many interations in the future
-Official reference for replicate weights: https://www.census.gov/content/dam/Census/library/publications/2020/acs/acs_pums_handbook_2020_ch05.pdf
+Reference for applying weights: https://www2.census.gov/programs-surveys/acs/tech_docs/pums/accuracy/2015_2019AccuracyPUMS.pdf
 """
 
 

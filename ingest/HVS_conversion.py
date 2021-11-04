3cfd1e8a1eb1c5819f8980a8d8a007483f452477
Alexander Weinstein <alexanderweinstein@DCP-APPLE-2153.local>
2021-11-04 10:39:39 -0400

    ready to start implementing Baiyue's solution as soon as I figure out how we link units to PUMAs. don't see where location information lives in HVS data

---
+++ ingest/HVS_conversion.py
@@ -0,0 +1,3 @@
+"""Based on Baiyue's work in 
+https://github.com/NYCPlanning/db-equitable-development-tool/issues/1
+I don't see any location information in this data, don't see how we link them to PUMS """

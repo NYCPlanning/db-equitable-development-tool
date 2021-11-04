3ba285c42da59e0ab55bfaa81cf92d7dca5e96de
Alexander Weinstein <alexanderweinstein@DCP-APPLE-2153.local>
2021-11-04 13:01:50 -0400

    started adding Baiyue's code to HVS conversion module

--- ingest/HVS_conversion.py
+++ ingest/HVS_conversion.py
@@ -1,3 +1,20 @@
 """Based on Baiyue's work in 
 https://github.com/NYCPlanning/db-equitable-development-tool/issues/1
-I don't see any location information in this data, don't see how we link them to PUMS """
+"""
+import requests
+import pandas as pd
+
+metadata_url = "https://www2.census.gov/programs-surveys/nychvs/datasets/2017/microdata/stata_import_program_17.txt"
+data_url = "https://www2.census.gov/programs-surveys/nychvs/datasets/2017/microdata/uf_17_occ_web_b.txt"
+
+
+def create_label_cleaner():
+    """Use metadata to Create dict that maps default column name
+    to human-readable column name."""
+    raw_2017_mapping = requests.get(metadata_url).text
+    occupied_labels = {}
+    for label in raw_2017_mapping.split("\n")[278:547]:
+        variable = label.replace("label variable ", "").split(" ")[0]
+        name = label.split('"')[1::2][0]
+        occupied_labels[variable] = name
+    return occupied_labels

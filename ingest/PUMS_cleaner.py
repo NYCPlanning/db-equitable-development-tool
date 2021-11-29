2dc39841d52e060d25e23d8b6a1d963222f57555
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2021-11-29 16:59:14 -0500

    catgeorizes INDP based on numeric ranges. Next add tests to make sure that categorizations are correct

--- ingest/PUMS_cleaner.py
+++ ingest/PUMS_cleaner.py
@@ -3,13 +3,53 @@ import pandas as pd
 import numpy as np
 from os.path import exists
 import requests
+import re
 
 
 class PUMSCleaner:
-    def __init__(self) -> None:
-        self.recode_df = self.get_recode_df()
 
-    def get_recode_df(self):
+    range_recode_fp = "resources/ACSPUMS2015_2019CodeLists.xlsx"
+
+    def __init__(self) -> None:
+        self.one_to_one_recodes = self.get_one_to_one_recode_df()
+        self.range_recodes = self.get_range_recodes()
+
+    def clean_simple_cateogorical(self, vi_data, column_name):
+        """For columns that are downloaded as integers and map one to one to categories in data dictionary"""
+        print(f"cleaning {column_name}")  # To-do: send this to log
+        codes = self.one_to_one_recodes[
+            self.one_to_one_recodes["variable_name"] == column_name
+        ]
+        codes = codes[["C", "Record Type"]]
+        # codes.replace({"C": {"b": 0}}, inplace=True)
+        # codes["C"] = codes["C"].astype(int)
+        codes["C"] = pd.to_numeric(codes["C"], errors="coerce").replace(np.NaN, 0)
+        codes.set_index("C", inplace=True)
+        mapper = {column_name: codes.to_dict()["Record Type"]}
+        vi_data[column_name] = vi_data[column_name].astype(int)
+        vi_data.replace(mapper, inplace=True)
+        return vi_data
+
+    def clean_continous(self, vi_data, column_name):
+        vi_data[column_name] = vi_data[column_name].astype(int)
+        return vi_data
+
+    def clean_range_categorical(self, vi_data, column_name):
+        """Based on https://stackoverflow.com/questions/57376325/replace-a-range-of-integer-values-in-multiple-columns-of-pandas"""
+
+        recode_ranges = self.range_recodes[column_name]
+        vi_data[column_name] = vi_data[column_name].astype(int)
+        new_col_name = column_name + "_cleaned"
+        vi_data[new_col_name] = None
+        for category in recode_ranges:
+            vi_data.loc[
+                vi_data[column_name].between(*category[0]), new_col_name
+            ] = category[1]
+        vi_data[column_name] = vi_data[new_col_name]
+        vi_data.drop(columns=[new_col_name])
+        return vi_data
+
+    def get_one_to_one_recode_df(self):
         fp = "resources/PUMS_recodes.csv"
         if not exists(fp):
             url = "https://www2.census.gov/programs-surveys/acs/tech_docs/pums/data_dict/PUMS_Data_Dictionary_2015-2019.csv"
@@ -24,20 +64,42 @@ class PUMSCleaner:
         recode_df.rename(columns={"level_1": "variable_name"}, inplace=True)
         return recode_df
 
-    def clean_simple_cateogorical(self, vi_data, column_name):
-        """For columns that are downloaded as integers and map one to one to categories in data dictionary"""
-        print(f"cleaning {column_name}")  # To-do: send this to log
-        codes = self.recode_df[self.recode_df["variable_name"] == column_name]
-        codes = codes[["C", "Record Type"]]
-        # codes.replace({"C": {"b": 0}}, inplace=True)
-        # codes["C"] = codes["C"].astype(int)
-        codes["C"] = pd.to_numeric(codes["C"], errors="coerce").replace(np.NaN, 0)
-        codes.set_index("C", inplace=True)
-        mapper = {column_name: codes.to_dict()["Record Type"]}
-        vi_data[column_name] = vi_data[column_name].astype(int)
-        vi_data.replace(mapper, inplace=True)
-        return vi_data
+    def get_range_recodes(self):
+        """For variables assigned to larger categories based on integer range"""
+        recodes = {}
+        recodes["INDP"] = self.column_recode(
+            sheet_name="Industry", recodes_column="Unnamed: 1", rows=range(2, 16)
+        )
+        recodes["OCCP"] = self.column_recode(
+            sheet_name="OCCP & SOCP", recodes_column="Unnamed: 2", rows=range(2, 7)
+        )
+        return recodes
 
-    def clean_continous(self, vi_data, column_name):
-        vi_data[column_name] = vi_data[column_name].astype(int)
-        return vi_data
+    def column_recode(self, sheet_name, recodes_column, rows):
+        industry_recodes = pd.read_excel(self.range_recode_fp, sheet_name=sheet_name)
+        industry_recodes.rename(columns={recodes_column: "Recode_Ranges"}, inplace=True)
+        rv = []
+        for r in rows:
+            recode_range = industry_recodes["Recode_Ranges"][r]
+            rv.extend(self.get_recode_mapper(recode_range.split(" ")[2:]))
+        return rv
+
+    def get_recode_mapper(self, l):
+        """Some categories span multiple discontinuous ranges"""
+        for i, v in enumerate(l):
+            if v and v[0].isalpha():
+                numeric_ranges = l[: i - 1]
+                category = " ".join(l[i:])
+                break
+        rv = []
+        for r in range(0, len(numeric_ranges), 3):
+            rv.append(
+                (
+                    (
+                        int(numeric_ranges[r]),
+                        int(re.sub("[^0-9]", "", numeric_ranges[r + 2])),
+                    ),
+                    category,
+                )
+            )
+        return rv

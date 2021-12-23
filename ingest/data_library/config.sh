3a4eb79140d4cb96f92c7216bccf29897e8b4176
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2021-12-23 18:30:48 -0500

    get datasets from data library with mini io

--- ingest/data_library/config.sh
+++ ingest/data_library/config.sh
@@ -9,3 +9,27 @@ function set_env {
 }
 # Set Environmental variables
 set_env .env 
+
+function get_version {
+  local name=$1
+  echo "name passed to get_version is $name"
+  local config_path=spaces/edm-recipes/datasets/$name/latest/config.json
+  local version=$(mc cat $config_path | jq -r '.dataset.version')
+  echo "$version"
+}
+
+function import_csv {
+  echo "downloading csv of dataset $1"
+  local name=$1
+  echo "name is $name"
+  local version=$(get_version $name)
+  echo "got version $version"
+  local target_dir=.library/datasets/$name/$version
+  if [ -f $target_dir/$name.csv ]; then
+    echo "✅ $name.csv exists in cache"
+  else
+    echo "🛠 $name.csv doesn't exists in cache, downloading ..."
+    mkdir -p $target_dir &&
+    mc cp spaces/edm-recipes/datasets/$name/latest/$name.csv $target_dir/$name.csv
+  fi
+}

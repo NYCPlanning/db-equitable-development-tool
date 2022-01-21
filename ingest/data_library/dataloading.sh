a6c1efd68ab201896ace58c284173ffaa94b08be
Te Du <tedudevelop@gmail.com>
2022-01-21 09:21:38 -0500

    second attempt to commit the resolved conflict branch

--- ingest/data_library/dataloading.sh
+++ ingest/data_library/dataloading.sh
@@ -2,8 +2,5 @@
 source ingest/data_library/config.sh
 
 import_csv lpc_historic_district_areas
-<<<<<<< HEAD
 import_csv dcp_housing
-=======
 import_csv doi_evictions
->>>>>>> 5e5a16eecdedd3d87484b6a327696b1194c9889f

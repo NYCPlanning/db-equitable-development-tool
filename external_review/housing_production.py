f3e23725cd8dbb44b7b6b354620bd4b48033206c
Te Du <tedudevelop@gmail.com>
2022-01-21 14:40:54 -0500

    set the geographies as index and make pd.concat work for any dataframes and create final output

--- external_review/housing_production.py
+++ external_review/housing_production.py
@@ -19,10 +19,18 @@ for g in subfolders:
     csvs = [os.path.join(path + '/' + g, file) for file in files if file.endswith('csv')]
 
     # have all the dataframes 
-    dfs = [pd.read_csv(csv) for csv in csvs]
+    dfs = [pd.read_csv(csv, index_col=[0]) for csv in csvs]
 
     # joined files 
-    merged = pd.concat(dfs, axis=1)
+    merged = pd.concat(dfs, axis=1, join='outer', ignore_index=False)
 
-    merged.to_csv('external_review/housing_production/' + g + '_merged.csv')
+    merged.reset_index(inplace=True)
+
+    if g == 'puma':
+
+        merged['index'] = merged['index'].apply(lambda x: '0' + str(x))
+    
+    merged.rename(columns={'index': 'geography'}, inplace=True)
+
+    merged.to_csv('external_review/housing_production/' + g + '_merged.csv', index=False)
  

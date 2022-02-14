d5391759839e3c04ebb0a13e2ad8d3cd740f6846
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-02-14 16:30:31 -0500

    works on my machine, Te's issues must be development environment

--- aggregate/PUMS/count_PUMS_economics.py
+++ aggregate/PUMS/count_PUMS_economics.py
@@ -20,11 +20,17 @@ class PUMSCountEconomics(PUMSCount):
             "civilian_employed_pop_filter",
         ),  # Termed "Employment by industry sector" in data matrix
         # apply civilian_employed_pop_filter
-        ("", ), 
+        # ("", ),
     ]
 
     def __init__(
-        self, limited_PUMA=False, year=2019, household=False, requery=False, add_MOE=True, keep_SE=False
+        self,
+        limited_PUMA=False,
+        year=2019,
+        household=False,
+        requery=False,
+        add_MOE=True,
+        keep_SE=False,
     ) -> None:
         self.crosstabs = ["race"]
         self.include_fractions = True
@@ -86,16 +92,16 @@ class PUMSCountEconomics(PUMSCount):
 
     def assign_to_household_income_band(self, person):
 
-        '''
-        turns out the NPF field is identiacal number of household members to all 
+        """
+        turns out the NPF field is identiacal number of household members to all
         individuals. So the individual assignment can be done fairly straight forwardsly
 
         the function then use the numpy digitize to put the household income into their respective bins
 
-        REMAINING QUESTION: PUMS aggregator then take this to the calculate_counts 
+        REMAINING QUESTION: PUMS aggregator then take this to the calculate_counts
         which needs to incorporate some ways to perform the calculation on the household levels
         which is different from counting on person level
-        '''
+        """
 
         income_bands = {
             1: [-9999999, 20900, 34835, 55735, 83602, 114952, 9999999],
@@ -105,15 +111,15 @@ class PUMSCountEconomics(PUMSCount):
             5: [-9999999, 32258, 53763, 86021, 129032, 177419, 9999999],
             6: [-9999999, 34636, 57727, 92362, 138544, 190498, 99999999],
             7: [-9999999, 37014, 61690, 98703, 148055, 203576, 99999999],
-            8: [-9999999, 39423, 65705, 105128, 157692, 216826, 99999999]
+            8: [-9999999, 39423, 65705, 105128, 157692, 216826, 99999999],
         }
 
-        labels = ['ELI', 'VLI', "LI", 'MI', 'MIDI', 'HI']
+        labels = ["ELI", "VLI", "LI", "MI", "MIDI", "HI"]
 
         idx = np.digitize(person["HINCP"], income_bands[person["NPF"]])
 
         return labels[idx - 1]
-        
+
     def civilian_employed_pop_filter(self, PUMS: pd.DataFrame):
         """Filter to return subset of all people ages 16-64 who are employed as civilians"""
         age_subset = PUMS[(PUMS["AGEP"] >= 16) & (PUMS["AGEP"] <= 64)]
--- statistical/calculate_counts.py
+++ statistical/calculate_counts.py
@@ -35,9 +35,10 @@ def calculate_counts(
 ):
     """To do: implement something more elegant than "a" dummy var"""
     data["a"] = 1
-    data[rw_cols] = data[rw_cols].replace({0: 0.01}) # this is a fix for replacate weight issue econ aggregator
+    data[rw_cols] = data[rw_cols].replace(
+        {0: 0.01}
+    )  # this is a fix for replacate weight issue econ aggregator
 
-    print(data)
     if crosstab:
         original_var = variable_col
         variable_col = f"{variable_col}-{crosstab}"

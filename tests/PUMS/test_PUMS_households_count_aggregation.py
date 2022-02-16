a6f8cb4ac2382a95f5eeede8b686f26c45a7f461
Te Du <tedudevelop@gmail.com>
2022-02-16 15:09:58 -0500

    add some basic test to the new household work

--- tests/PUMS/test_PUMS_households_count_aggregation.py
+++ tests/PUMS/test_PUMS_households_count_aggregation.py
@@ -14,40 +14,24 @@ def test_local_loader(all_data):
     local_loader.load_aggregated_counts(all_data, type="households")
 
 
+
 @pytest.mark.test_aggregation
-def test_all_income_bands_count_sum_total_puma():
-    """Can be folded into test_that_all_races_sum_to_total_within_indicator in demographics aggregation tests"""
-    lf_race_cols = [f"lf_{r}" for r in race_counts]
-    ib_cols = ['ELI', 'VLI', "LI", 'MI', 'MIDI', 'HI']
+def test_industry_assigned_correctly():
+    """Can parameterize this to include other industries"""
     assert (
-        local_loader.aggregated[lf_race_cols].sum(axis=1)
-        == local_loader.aggregated["lf-count"]
+        local_loader.by_person[(local_loader.by_person["HINCP"] == '50000') & (local_loader.by_person["NPF"] == '5')][
+            "household_income_bands"
+        ]
+        == "LI"
     ).all()
-
-
-@pytest.mark.test_aggregation
-def test_all_occupations_and_industry_sum_to_total():
-    """This is harder, have to reflect on how to do this"""
-    pass
 
 
 @pytest.mark.test_aggregation
 def test_industry_assigned_correctly():
     """Can parameterize this to include other industries"""
     assert (
-        local_loader.by_person[(local_loader.by_person["HINCP"] == '8000') & (local_loader.by_person["NPF"] == '3')][
+        local_loader.by_person[(local_loader.by_person["HINCP"] == '100000') & (local_loader.by_person["NPF"] == '2')][
             "household_income_bands"
         ]
-        == "ELI"
-    ).all()
-
-
-@pytest.mark.test_aggregation
-def test_occupation_assigned_correctly():
-    """Can parameterize this to include other occupations"""
-    assert (
-        local_loader.by_person[
-            local_loader.by_person["OCCP"] == "Sales and Office Occupations"
-        ]["occupation"]
-        == "slsoff"
+        == "MIDI"
     ).all()

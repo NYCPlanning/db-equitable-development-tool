6fea9506b88bd0386db31e88d6d35940c06cf6a0
Te Du <tedudevelop@gmail.com>
2022-02-24 11:20:07 -0500

    create ingest

--- aggregate/quality_of_life/self_reported_health.py
+++ aggregate/quality_of_life/self_reported_health.py
@@ -1,6 +1,22 @@
 import pandas as pd
 
-def self_reported_health():
+def ingest(geo: str):
 
-    df = pd.read_excel('resouces/quality_of_life/selfreportedheal.xlsx'
-        sheet_name='Cit')
+    sheetname = {
+        'citywide': 'Citywide_race+eth',
+        'boro': 'Borough_race+eth',
+        'puma': 'UHF'
+    }
+
+    df = pd.read_excel('resouces/quality_of_life/selfreportedheal.xlsx',
+        sheet_name=sheetname[geo], 
+        usecols=['Yearnum', 'Response', 'Dimension Response', 'Estunated Population', 'Estimated Prevalence']
+    ) 
+
+    return df
+
+def self_reported_health(geo:str,):
+
+    df = ingest(geo)
+
+    return 

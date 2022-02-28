20672e8c02ac6f8912768e6789f8f6ad71239f0a
Te Du <tedudevelop@gmail.com>
2022-02-28 12:25:23 -0500

    finished covid death indicator

---
+++ aggregate/quality_of_life/covid_death.py
@@ -0,0 +1,70 @@
+from typing import final
+import pandas as pd
+from utils.PUMA_helpers import puma_to_borough
+from internal_review.set_internal_review_file import set_internal_review_files
+import numpy as np
+
+race_coder = {
+    'Asian/Pacific Islander': '_anh', 
+    'Black/African American': '_bnh', 
+    'Hispanic/Latino': '_hsp', 
+    'Other/Unknown': '_onh', 
+    'White': '_wnh'
+}
+
+
+def covid_death(geography: str, write_to_internal_review=False):
+    """this is used to create the final dataframe and write final output to internal review files"""
+
+    assert geography in ["citywide", "borough", "puma"]
+    indicator_col_label = 'total_covid_death'
+
+    clean_df = load_clean_source_data()
+    #print(clean_df)
+
+    if geography in ['citywide', 'borough']:
+
+        agg = clean_df.groupby([geography, 'race']).sum(numeric_only=True).reset_index()
+        final = agg.pivot(index=geography, columns='race', values='total_covid_death')
+        
+    elif geography == 'puma':
+        # handle the and also censored data by replace them with null
+        final = clean_df.pivot(index='puma', columns='race', values='total_covid_death')#.replace('*', np.nan)
+
+    # create the total without race breakdown
+    final[''] = final.sum(axis=1)
+
+    for col in final.columns:
+        final.rename(columns={col: indicator_col_label + col}, inplace=True)
+
+    # rename index and redy for output
+    final.index.rename(geography, inplace=True)
+    #final.reset_index(inplace=True)
+
+    if write_to_internal_review:
+        set_internal_review_files(
+            [(final, 'total_covid_death.csv', geography)],
+            category='quality_of_life'
+        )
+
+    return final
+
+def load_clean_source_data():
+
+    source_data = pd.read_excel(
+        "resources/quality_of_life/Deaths.by.race.and.PUMA_20220202.xlsx",
+        sheet_name='Sheet 1',
+        header=3,
+    )
+    #print(source_data)
+    source_data.rename(columns={'PUMA': 'puma', 'Total\nDeaths': 'total_covid_death', 'Race/Ethnicity': 'race'}, inplace=True)
+    source_data['race'] = source_data['race'].map(race_coder)
+    source_data['puma'] = '0' + source_data['puma'].astype(str)
+    # create the geographies to aggregate on
+    source_data['citywide'] = 'citywide'
+    source_data['borough'] = source_data.apply(axis=1, func=puma_to_borough)
+    #handle the censored data point by replacing them with NaN
+    source_data['total_covid_death'] = source_data['total_covid_death'].replace('*', np.nan)
+
+    return source_data
+
---
+++ utils/PUMA_helpers.py
@@ -0,0 +1,124 @@
+import geopandas as gp
+from shapely.geometry import Point
+import pandas as pd
+from numpy import add, nan
+import usaddress
+import requests
+from geosupport import Geosupport, GeosupportError
+from ingest.ingestion_helpers import add_leading_zero_PUMA
+from utils.geocode import from_eviction_address
+import re
+
+geocode_functions = {"from_eviction_address": from_eviction_address}
+
+borough_code_mapper = {
+    "037": "BX",
+    "038": "MN",
+    "039": "SI",
+    "040": "BK",
+    "041": "QN",
+}
+
+
+def puma_to_borough(record):
+
+    borough_code = record.puma[:3]
+    borough = borough_code_mapper[borough_code]
+    return borough
+
+
+NYC_PUMAS_url = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Public_Use_Microdata_Areas_PUMAs_2010/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson"
+
+
+def NYC_PUMA_geographies() -> gp.GeoDataFrame:
+    res = requests.get(
+        "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Public_Use_Microdata_Areas_PUMAs_2010/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson"
+    )
+    gdf = gp.GeoDataFrame.from_features(res.json()["features"])
+    gdf.rename(columns={"PUMA": "puma"}, inplace=True)
+    gdf = add_leading_zero_PUMA(gdf)
+    return gdf
+
+
+PUMAs = NYC_PUMA_geographies()
+
+
+def assign_PUMA_col(df: pd.DataFrame, lat_col, long_col, geocode_process=None):
+    df.rename(columns={lat_col: "latitude", long_col: "longitude"}, inplace=True)
+    df["puma"] = df.apply(assign_PUMA, axis=1, args=(geocode_process,))
+    print(f"got {df.shape[0]} evictions to assign PUMAs to ")
+    print(f"assigned PUMAs to {df['puma'].notnull().sum()}")
+    return df
+
+
+def assign_PUMA(record: gp.GeoDataFrame, geocode_process):
+    if pd.notnull(record.latitude) and pd.notnull(record.longitude):
+        return PUMA_from_coord(record)
+    if geocode_process:
+        return geocode_functions[geocode_process]
+
+
+def PUMA_from_coord(record):
+    """Don't think I need to make a geodata frame here, shapely object would do"""
+
+    record_loc = Point(record.longitude, record.latitude)
+    matched_PUMA = PUMAs[PUMAs.geometry.contains(record_loc)]
+    if matched_PUMA.empty:
+        return None
+    return matched_PUMA.puma.values[0]
+
+
+def community_district_to_PUMA(df, CD_col):
+    """First two operations to read excel and clean columns are from Te's education pull request.
+    Can be DRY'd out"""
+    puma_cross = pd.read_excel(
+        "https://www1.nyc.gov/assets/planning/download/office/data-maps/nyc-population/census2010/nyc2010census_tabulation_equiv.xlsx",
+        sheet_name="NTA in PUMA_",
+        header=6,
+        dtype=str,
+    )
+    puma_cross.columns = puma_cross.columns.str.replace(" \n", "")
+
+    mapper = {}
+
+    puma_cross.rename(
+        columns={
+            "Community District(PUMAs approximate NYC Community  Districts and are not coterminous)": "CD"
+        },
+        inplace=True,
+    )
+    puma_cross.PUMACode = "0" + puma_cross.PUMACode
+
+    for _, row in puma_cross.iterrows():
+        for cd_num in re.findall(r"\d+", row["CD"]):
+            cd_code = row["CD"][:2] + cd_num
+            mapper[cd_code] = row["PUMACode"]
+
+    df["puma"] = df[CD_col].replace(mapper)
+    return df
+
+def get_all_NYC_PUMAs():
+    """Adopted from code in PUMS_query_manager"""
+    geo_ids = [
+        range(4001, 4019),  # Brooklyn
+        range(3701, 3711),  # Bronx
+        range(4101, 4115),  # Queens
+        range(3901, 3904),  # Staten Island
+        range(3801, 3811),  # Manhattan
+    ]
+    rv = []
+    for borough in geo_ids:
+        rv.extend(["0" + str(PUMA) for PUMA in borough])
+    return rv
+
+
+def get_all_boroughs():
+    return ["BK", "BX", "MN", "QN", "SI"]
+
+
+def clean_PUMAs(puma) -> pd.DataFrame:
+    """Re-uses code from remove_state_code_from_PUMA col in access to subway, call this instead"""
+    puma = str(puma)
+    if puma[:2] == "36":
+        puma = puma[2:]
+    return puma

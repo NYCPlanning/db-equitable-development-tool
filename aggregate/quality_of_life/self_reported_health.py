4529ad47ebb1f5050a6b3268a5294782fd980124
Te Du <tedudevelop@gmail.com>
2022-02-28 16:16:00 -0500

    puma crosswalk created but still not working

--- aggregate/quality_of_life/self_reported_health.py
+++ aggregate/quality_of_life/self_reported_health.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from utils.PUMA_helpers import community_district_to_PUMA
+from internal_review.set_internal_review_file import set_internal_review_files
 
 race = {
     'Asian/Pacific islander': 'anh', 
@@ -28,27 +29,34 @@ def self_reported_health(geography:str, write_to_internal_review=False):
 
     assert geography in ["citywide", "borough", "puma"]
 
-    df = load_clean_source_data(geography)
+    health_data = load_clean_source_data(geography)
 
     if geography == 'citywide':
-        df['citywide'] = 'citywide'
-        df_ct = df.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
-        df_pct = df.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
+        health_data['citywide'] = 'citywide'
+        health_data_ct = health_data.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
+        health_data_pct = health_data.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
     elif geography == 'boro':
-        df.rename(columns={'Dimension Response': 'boro', 'Dim2Value': 'ethnicity', })
-        df['boro'] = df['boro'].map(boro_mapper)
-        df.rename(columns={'Dimension': 'boro', 'Dim2Value': 'ethnicity'})
-        df_ct = df.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
-        df_pct = df.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
+        health_data.rename(columns={'Dimension Response': 'boro', 'Dim2Value': 'ethnicity', })
+        health_data['boro'] = health_data['boro'].map(boro_mapper)
+        health_data.rename(columns={'Dimension': 'boro', 'Dim2Value': 'ethnicity'})
+        health_data_ct = health_data.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
+        health_data_pct = health_data.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
     else:
-        print(df)
+        #print(health_data)
+        health_data = community_district_to_PUMA(health_data, 'ID')
+        
 
     #final = transform(health_data, geo)
 
     #final.to_csv('internal_review/quality_of_life/self_report_health.csv', index=False)
 
-    final = df
+    final = health_data
 
+    if write_to_internal_review:
+        set_internal_review_files(
+            [(final, "pedestrian_hospitalizations.csv", geography)],
+            category="quality_of_life",
+        )
     return final
 
 def load_clean_source_data(geo: str):
@@ -77,63 +85,63 @@ def load_clean_source_data(geo: str):
             'Estimated Prevalence'
         ]
     else:
-        df = pd.read_excel('resources/quality_of_life/2018-chp-pud.xlsx',
+        health_data = pd.read_excel('resources/quality_of_life/2018-chp-pud.xlsx',
             sheet_name='CHP_all_data',
             header=1,
-            usecols="FM:FO"
+            usecols="A,FM:FO"
         )
-        return df
+        return health_data
     #if geo in ['citywide', 'boro']:
-    df = pd.read_excel('resources/quality_of_life/selfreportedhealth.xlsx',
+    health_data = pd.read_excel('resources/quality_of_life/selfreportedhealth.xlsx',
         sheet_name=sheetname[geo], 
         usecols=var_ls
     ) 
 
-    return df
+    return health_data
 
-def transform(df: pd.DataFrame, geo: str,):
+def transform(health_data: pd.DataFrame, geo: str,):
 
     if geo == 'citywide':
-        df['citywide'] = 'citywide'
-        df_ct = df.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
-        df_pct = df.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
+        health_data['citywide'] = 'citywide'
+        health_data_ct = health_data.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
+        health_data_pct = health_data.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
     elif geo == 'boro':
-        df.rename(columns={'Dimension Response': 'boro', 'Dim2Value': 'ethnicity', })
-        df['boro'] = df['boro'].map(boro_mapper)
-        df.rename(columns={'Dimension': 'boro', 'Dim2Value': 'ethnicity'})
-        df_ct = df.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
-        df_pct = df.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
+        health_data.rename(columns={'Dimension Response': 'boro', 'Dim2Value': 'ethnicity', })
+        health_data['boro'] = health_data['boro'].map(boro_mapper)
+        health_data.rename(columns={'Dimension': 'boro', 'Dim2Value': 'ethnicity'})
+        health_data_ct = health_data.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
+        health_data_pct = health_data.pivot(index='boro', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Prevalence')
     elif geo == 'puma':
-        cds = df['Dimension Response'].split(' ')[0].split('/')
+        cds = health_data['Dimension Response'].split(' ')[0].split('/')
         
         
         pumacrosser = community_district_to_PUMA()
-        df.rename
+        health_data.rename
     else:
         print('invalid geography input')
 
-    final = pd.concat([df_ct, df_pct], )
-    #pop = pd.melt(df, id_vars=['Yearnum', 'Response', 'Dimension Response'], var_name='Estimated Population', )
+    final = pd.concat([health_data_ct, health_data_pct], )
+    #pop = pd.melt(health_data, id_vars=['Yearnum', 'Response', 'Dimension Response'], var_name='Estimated Population', )
     
-    perc = pd.melt(df, id_vars=['Yearnum', 'Response', 'Dimension Response'], var_name='Estimated Prevalence', )
+    perc = pd.melt(health_data, id_vars=['Yearnum', 'Response', 'Dimension Response'], var_name='Estimated Prevalence', )
 
     #pop.columns = ['Yearnum', 'Response', 'Dimension Response']
 
     #print(pop)
 
-    print(df.Response.unique())
-    #df.Response = df.Response.map()
-    print(df.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population'))
+    print(health_data.Response.unique())
+    #health_data.Response = health_data.Response.map()
+    print(health_data.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population'))
 
-    p_df = df.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
+    p_health_data = health_data.pivot(index='citywide', columns=['Yearnum', 'Response', 'Dimension Response'], values='Estimated Population')
 
-    #print(p_df.columns)
+    #print(p_health_data.columns)
 
-    #pd.crosstab(df, )
+    #pd.crosstab(health_data, )
 
-    #p_df.columns = ['_'.join(tups) for tups in p_df.columns]
+    #p_health_data.columns = ['_'.join(tups) for tups in p_health_data.columns]
 
-    final = p_df
+    final = p_health_data
 
     #print(pop)
 
@@ -141,6 +149,6 @@ def transform(df: pd.DataFrame, geo: str,):
 
     return final
 
-def rename_fields(df: pd.DataFrame):
+def rename_fields(health_data: pd.DataFrame):
 
     return 

3d236d700601d5886a438884f8fb1b6722f4c10b
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-03 10:51:05 -0500

    new column headers, numbers remain the same

--- aggregate/quality_of_life/access_transit.py
+++ aggregate/quality_of_life/access_transit.py
@@ -4,13 +4,17 @@
 """
 import pandas as pd
 from internal_review.set_internal_review_file import set_internal_review_files
-from utils.assign_PUMA import puma_to_borough
+from utils.PUMA_helpers import puma_to_borough
 
 
 def access_subway_and_access_ADA(geography, save_for_internal_review=False):
     """Accessor for two similar indicators:
     - Percent of residents within 1/4 mile of ADA accessible subway stations
     - Percent within 1/4 mile of subway or Select Bus station"""
+
+    assert geography in ["puma", "borough", "citywide"]
+    subway_SBS_ind_name = "access_subwaysbs_pct"
+    ADA_ind_name = "access_ADA_pct"
 
     access_subway_SBS = load_access_subway_SBS()
     access_ADA_subway = load_access_ADA_subway()
@@ -19,10 +23,13 @@ def access_subway_and_access_ADA(geography, save_for_internal_review=False):
     assign_geo_cols(access_ADA_subway)
 
     subway_fraction = calculate_access_fraction(
-        access_subway_SBS, geography, "pop_with_access_subway_SBS", "access-subway-pct"
+        access_subway_SBS,
+        geography,
+        "pop_with_access_subway_SBS",
+        subway_SBS_ind_name,
     )
     ADA_fraction = calculate_access_fraction(
-        access_ADA_subway, geography, "pop_with_accessible_ADA_subway", "access-ADA-pct"
+        access_ADA_subway, geography, "pop_with_accessible_ADA_subway", ADA_ind_name
     )
     subway_and_ADA_access = subway_fraction.merge(
         ADA_fraction, left_index=True, right_index=True
@@ -32,7 +39,7 @@ def access_subway_and_access_ADA(geography, save_for_internal_review=False):
             access_df=subway_and_ADA_access, geography=geography
         )
 
-    return subway_and_ADA_access
+    return subway_and_ADA_access[[subway_SBS_ind_name, ADA_ind_name]]
 
 
 def assign_geo_cols(access_dataset):

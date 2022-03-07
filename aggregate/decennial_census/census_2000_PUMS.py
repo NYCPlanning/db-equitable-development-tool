5d631f344bedb11c3dfd2f0428c1b0c42594f1ce
Max <max@DCP-APPLE-1991.local>
2022-03-07 16:55:22 -0500

    update python script to handle column renaming and add new exploratory python notebook

--- aggregate/decennial_census/census_2000_PUMS.py
+++ aggregate/decennial_census/census_2000_PUMS.py
@@ -7,6 +7,24 @@ breakdowns]).
 
 import pandas as pd
 
+col_new = {
+    ## Rename the demographic race columns with wiki conventions
+    "a00": "00_anh",
+    "b00": "00_bnh",
+    "h00": "00_hsp",
+    "o00": "00_onh",
+    "w00": "00_wnh",
+    ### Standardize total pop and popu16 column headers
+    "pop": "total_pop",
+    "pu16": "popu16",
+    ### Standardize suffix for columns
+    "e$": "_est",
+    "m$": "_moe",
+    "c$": "_cv",
+    "p$": "_pct",
+    "z$": "_zscore",
+}
+
 
 def load_dec_2000_demographic_pop_demo():
     df = pd.read_excel(
@@ -37,20 +55,29 @@ def filter_to_demo_indicators(df):
     return df
 
 
-def remove_duplicate_cols_z_score_cols(df):
+def remove_duplicate_cols(df):
     """Excel spreadsheet has some duplicate columns that Erica used for calculations
-    and she adds an unncesserary column for z score which should be removed for downstream
-    digital services application team"""
-    df = df.drop(df.filter(regex="Z$|E.1$|M.1$|C.1$|P.1$|Z.1$").columns, axis=1)
+    - keep columns ending with Z for now"""
+    df = df.drop(df.filter(regex="E.1$|M.1$|C.1$|P.1$|Z.1$").columns, axis=1)
     return df
 
 
 def rename_columns(df):
+    """Rename column headers to lower case and pass the dictionary to replace values from Erica's file"""
     df.columns = map(str.lower, df.columns)
-    df.columns = str.replace()
+    df.columns = df.columns.to_series().replace(col_new, regex=True)
+
+    return df
 
 
 def create_geo_level_df(df):
     df_citywide = df.loc["citywide"]
     df_borough = df.loc[["BX", "BK", "MN", "QN", "SI"]]
     df_puma = df.loc[3701:4114]
+    print(
+        "Shape of new dataframes - df_citwide{} , df_borough{}, df_puma{}".format(
+            df_citywide.shape, df_borough.shape, df_puma.shape
+        )
+    )
+
+    return df_citywide, df_borough, df_puma

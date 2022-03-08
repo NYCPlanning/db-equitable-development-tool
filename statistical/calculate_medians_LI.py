4744b4a27fdab57f3f2ef2928730bb97657d87bd
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-08 11:49:23 -0500

    calculate median LI returns dataframe where rows are geographies and columns are medians, variance measures

--- statistical/calculate_medians_LI.py
+++ statistical/calculate_medians_LI.py
@@ -3,12 +3,21 @@ import json
 import pandas as pd
 
 
-def calculate_median_LI(indicator_name, PUMS):
+def calculate_median_LI(PUMS, indicator_name, geo_col):
     bin_dict = lookup_bins(indicator_name)
-    bin_counts = frequency_per_bin(indicator_name, PUMS, bin_dict)
-    bin_counts["cum_sum"] = bin_counts.cumsum(axis=0)
-    bin_counts["cum_pct"] = bin_counts["cum_sum"] / bin_counts["frequency"].sum()
-    return calculate(bin_counts=bin_counts, bin_dict=bin_dict)
+    final = pd.DataFrame(
+        index=PUMS[geo_col].unique(),
+        columns=[
+            f"{indicator_name}-median",
+            f"{indicator_name}-median-moe",
+            f"{indicator_name}-median-cv",
+        ],
+    )
+    geo_bin_counts = frequency_per_bin_geo(PUMS, indicator_name, geo_col, bin_dict)
+    for puma, bin_counts in geo_bin_counts.groupby(level=0):
+        bin_counts["cum_sum"] = bin_counts.cumsum(axis=0)
+        final.loc[puma] = calculate(bin_counts=bin_counts, bin_dict=bin_dict)
+    return final
 
 
 def calculate(bin_counts, bin_dict):
@@ -34,10 +43,12 @@ def calculate(bin_counts, bin_dict):
         median = lower_bound_of_median_containing_bin + (
             (N / 2) - cum_sum_lower_bins
         ) * (width_median_containing_bin / frequency_of_median_containing_bin)
-    return median
+    MOE = None
+    CV = None
+    return median, MOE, CV
 
 
-def frequency_per_bin(indicator_name, PUMS: pd.DataFrame, bins: dict):
+def frequency_per_bin_geo(PUMS: pd.DataFrame, indicator_name, geo_col, bins: dict):
     labels = []
     ranges = [0]
     for label, range in bins.items():
@@ -48,7 +59,7 @@ def frequency_per_bin(indicator_name, PUMS: pd.DataFrame, bins: dict):
     PUMS["bin"] = pd.cut(
         PUMS[indicator_name], bins=ranges, labels=labels, include_lowest=True
     )
-    gb = PUMS.groupby("bin").sum()[["PWGTP"]]
+    gb = PUMS.groupby([geo_col, "bin"]).sum()[["PWGTP"]]
     return gb.rename(columns={"PWGTP": "frequency"})
 
 

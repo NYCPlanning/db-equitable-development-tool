e1e1b54ee60f471c82dfec19a5ab4a15e912cbcf
Max <max@DCP-APPLE-1991.local>
2022-03-10 11:15:42 -0500

    update census pums file for debug

--- aggregate/decennial_census/census_2000_PUMS.py
+++ aggregate/decennial_census/census_2000_PUMS.py
@@ -1,6 +1,6 @@
 """This script takes the xlsx files Erica from DCP Population sent us imports them, 
 cleans the column names to match our column schema naming conventions, filters out 
-unnecessary columns (z score columns, the Housing Security and Quality Indicators 
+unnecessary columns (the Housing Security and Quality Indicators 
 [Total Occupied Units, Owner Occupied, Renter Occupied and the corresponding racial 
 breakdowns]). 
 """
@@ -10,17 +10,22 @@ from sqlalchemy import column
 from utils.PUMA_helpers import clean_PUMAs
 from internal_review.set_internal_review_file import set_internal_review_files
 
-col_new = {
+demo_suffix = {
     ## Rename the demographic race columns with wiki conventions
-    "a00": "00_anh",
-    "b00": "00_bnh",
-    "h00": "00_hsp",
+    "a00": "anh",
+    "b00": "bnh",
+    "h00": "hsp",
     #   "o00": "00_onh", No other non hispanic in excel file
-    "w00": "00_wnh",
-    ### Standardize total pop and popu16 column headers
-    "pop": "total_pop",
-    "pu16": "popu16",
-    ### Standardize suffix for columns
+    "w00": "wnh",
+}
+
+
+### Standardize total pop and popu16 column headers
+standardize_pop = {"pop_00": "pop", "pu16": "popu16", "MdAge": "age_median"}
+
+### Standardize suffix for columns
+stat_suffix = {
+    "00": "",
     "e$": "_est",
     "m$": "_moe",
     "c$": "_pct_cv",
@@ -55,7 +60,9 @@ def filter_to_demo_indicators(df):
     """Remove columns that don't pretain to demographic indicators (Occupied units,
     renter occupied units, owner occupied units which come 200 ACS PUMS Data but are
     Housing Security and Quality category indicators"""
-    df = df.drop(df.filter(regex="Occ|OOcc|ROcc").columns, axis=1)
+    df = df.drop(
+        df.filter(regex="P25pl|LTHS|HSGrd|SClgA|BchD|Occ|OOcc|ROcc").columns, axis=1
+    )
     return df
 
 
@@ -69,7 +76,22 @@ def remove_duplicate_cols(df):
 def rename_cols(df):
     """Rename column headers to lower case and pass the dictionary to replace values from Erica's file"""
     df.columns = map(str.lower, df.columns)
-    df.columns = df.columns.to_series().replace(col_new, regex=True)
+    df.columns = df.columns.to_series().replace(demo_suffix, regex=True)
+
+    # df.columns = df.columms.to_series().replace(standardize_pop, regex=True)
+    # df.columns = df.columms.to_series().replace(stat_suffix, regex=True)
+
+    return df
+
+
+def rename_pop_cols(df):
+    df.columns = df.columns.to_series().replace(standardize_pop, regex=True)
+
+    return df
+
+
+def rename_stat_cols(df):
+    df.columns = df.columms.to_series().replace(stat_suffix, regex=True)
 
     return df
 

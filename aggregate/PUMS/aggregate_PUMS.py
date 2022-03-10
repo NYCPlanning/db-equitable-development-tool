c799b410ceea08d28240262a89ca8ec61e8a08a0
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-10 16:05:26 -0500

    bring in count cv

--- aggregate/PUMS/aggregate_PUMS.py
+++ aggregate/PUMS/aggregate_PUMS.py
@@ -214,7 +214,7 @@ class PUMSAggregator(BaseAggregator):
         Probably a cleaner way to handle cases where categories have specific order
         """
         if indicator == "age_bucket":
-            self.categories["age_bucket"] = ["popu16", "p16t64", "p65pl"]
+            self.categories["age_bucket"] = ["age_popu16", "age_p16t64", "age_p65pl"]
         elif indicator == "household_income_bands":
             self.categories["household_income_bands"] = [
                 "ELI",
@@ -249,7 +249,7 @@ class PUMSAggregator(BaseAggregator):
             for ind_category in self.categories[ind]:
                 for measure in ["", "_pct"]:
                     col_order.append(f"{ind_category}{measure}")
-                    if measure == "count":
+                    if measure == "":
                         col_order.append(f"{ind_category}{measure}_cv")
                     col_order.append(f"{ind_category}{measure}_moe")
                 col_order.append(f"{ind_category}_pct_denom")
@@ -261,7 +261,7 @@ class PUMSAggregator(BaseAggregator):
                                 f"{ind_category}_{race_crosstab}{measure}"
                             )
                             col_order.append(f"{column_label_base}")
-                            if measure == "count":
+                            if measure == "":
                                 col_order.append(f"{column_label_base}_cv")
                             col_order.append(f"{column_label_base}_moe")
                         col_order.append(f"{ind_category}_{race_crosstab}_pct_denom")

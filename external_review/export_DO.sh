a913dad5456b86ba141e3c823c6a7051e5135641
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-11 14:22:04 -0500

    cleanup unused comments

--- external_review/export_DO.sh
+++ external_review/export_DO.sh
@@ -24,7 +24,3 @@ case $1 in
     export ) export_DO $2 $3 ;;
     export_PUMS ) export_DO_PUMS $2 $3 $4
 esac
-
-# export_DO housing_production_citywide.csv housing_production
-# export_DO housing_production_borough.csv housing_production
-# export_DO housing_production_puma.csv housing_production

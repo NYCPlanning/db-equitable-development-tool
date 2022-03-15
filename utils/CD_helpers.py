3043d090a6cece85be013e430b0354b1cb8097cf
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-15 11:36:09 -0400

    get 3 digit puma code

--- utils/CD_helpers.py
+++ utils/CD_helpers.py
@@ -18,6 +18,21 @@ def get_CD_puma_crosswalk():
         header=6,
         dtype=str,
     )
+    puma_cross.columns = puma_cross.columns.str.replace(" \n", "")
+    puma_cross.rename(
+        columns={
+            "Unnamed: 0": "borough",
+            "Unnamed: 1": "county_code",
+            "Unnamed: 2": "borough_code",
+        },
+        inplace=True,
+    )
+    puma_cross.rename(
+        columns={
+            "Community District(PUMAs approximate NYC Community  Districts and are not coterminous)": "CD"
+        },
+        inplace=True,
+    )
 
     return puma_cross
 
@@ -26,16 +41,9 @@ def community_district_to_PUMA(df, CD_col):
     """First two operations to read excel and clean columns are from Te's education pull request.
     Can be DRY'd out"""
     puma_cross = get_CD_puma_crosswalk()
-    puma_cross.columns = puma_cross.columns.str.replace(" \n", "")
 
     mapper = {}
 
-    puma_cross.rename(
-        columns={
-            "Community District(PUMAs approximate NYC Community  Districts and are not coterminous)": "CD"
-        },
-        inplace=True,
-    )
     puma_cross.PUMACode = "0" + puma_cross.PUMACode
 
     for _, row in puma_cross.iterrows():
@@ -45,3 +53,13 @@ def community_district_to_PUMA(df, CD_col):
 
     df["puma"] = df[CD_col].replace(mapper)
     return df
+
+
+def three_digit_CD_to_puma():
+    puma_cross = get_CD_puma_crosswalk()
+    puma_cross["borough_abbr"] = puma_cross["borough"].map(borough_name_mapper)
+    puma_cross["CD_code"] = puma_cross["CD"].str.extract(r"(\d+)").astype(str)
+    puma_cross["CD_code"] = puma_cross["CD_code"].apply(
+        lambda x: "0" + x if len(x) == 1 else x
+    )
+    puma_cross["CD_code"] = puma_cross["borough_abbr"] + puma_cross["CD_code"]

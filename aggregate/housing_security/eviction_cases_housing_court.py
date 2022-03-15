62c16814391bf07d0097741c5c95d5af5bff4eac
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-15 15:50:28 -0400

    working on CD to puma

--- aggregate/housing_security/eviction_cases_housing_court.py
+++ aggregate/housing_security/eviction_cases_housing_court.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
+from utils.PUMA_helpers import clean_PUMAs
+
 
 def load_filings():
     filings = pd.read_excel(
         "resources/housing_security/eviction_filings.xlsx", skiprows=4, nrows=59
     )
+    filings["Community District"] = filings["Comm"]
     filings["citywide"] = "citywide"
-
+    # filings["puma"] = filings["puma"].apply(clean_PUMAs)
     return filings
 
 

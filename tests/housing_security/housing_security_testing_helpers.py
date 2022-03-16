48d497e974b84be1af652bdbf84f496a3fee535f
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-03-16 16:24:03 -0400

    take out three or more maintenance deficencies

--- tests/housing_security/housing_security_testing_helpers.py
+++ tests/housing_security/housing_security_testing_helpers.py
@@ -1,11 +1,11 @@
 """Currently count residential evictions is left out as it requires geocoding, can address this later if need be.
 Three or more maintenance deficiences also left out as I'm not sure where we are getting data moving forward"""
 
-from aggregate.housing_security.three_or_more_maintenance_deficiencies import (
-    count_units_three_or_more_deficiencies,
-)
-from aggregate.housing_security.evictions_by_city_marshals import (
-    count_residential_evictions,
-)
+# from aggregate.housing_security.three_or_more_maintenance_deficiencies import (
+#     count_units_three_or_more_deficiencies,
+# )
+# from aggregate.housing_security.evictions_by_city_marshals import (
+#     count_residential_evictions,
+# )
 
 accessors = []

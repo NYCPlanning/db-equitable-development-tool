52bd364bd1d89f70cceee6cabaa08837f5972b78
Te Du <tedudevelop@gmail.com>
2022-03-17 11:09:36 -0400

    add measuremrent renaming

--- aggregate/housing_security/rent_stable_three_mainteinance.py
+++ aggregate/housing_security/rent_stable_three_mainteinance.py
@@ -1,82 +1,76 @@
 from typing import final
+from bleach import clean
 import pandas as pd
 from utils.PUMA_helpers import clean_PUMAs
 from internal_review.set_internal_review_file import set_internal_review_files
 from aggregate.clean_aggregated import order_affordable
-from utils.PUMA_helpers import (
-    community_district_to_PUMA,
-    clean_PUMAs,
-    borough_name_mapper,
-)
-
-
-ind_mapper = {
-    "Af": "units_affordable_",
-    "ROcc2": "units_renteroccu"
-}
-
-income_mapper = {
-    "ELI": "eli",
-    "VLI": "vli",
-    "LI": "li",
-    "MI": "mi",
-    "Midi": "midi",
-    "HI": "hi",
-}
+from utils.PUMA_helpers import borough_name_mapper, clean_PUMAs
 
 suffix_mapper = {
-    "_19E": "",
-    "_19M": "_moe",
-    "_19C": "_cv",
-    "_19P": "_pct",
-    "_19Z": "_pct_moe",
+    "_N": "",
+    "Percent MOE\n(95% CI)": "pct_moe", # don't love this. But the order does matter here. As the MOE is a partial string match
+    "MOE\n(95% CI)": "moe",
+    "CV": "cv",
+    "Percent": "pct",
 }
 
 def rent_stablized_units(
     geography: str, write_to_internal_review=False
     ) -> pd.DataFrame:
     clean_data = load_source_clean_data("units_rentstable")
+    
+    if geography == "puma":
+        clean_data["puma"] = clean_data["PUMA"].apply(func=clean_PUMAs)
+        final = clean_data.loc[~clean_data.puma.isna()]
+    elif geography == "borough":
+        clean_data["borough"] = clean_data["CD Name"].map(borough_name_mapper)
+        final = clean_data.loc[~clean_data.borough.isna()].copy()
+    else:
+        clean_data.loc[clean_data["CD Name"] == "NYC", "citywide"] = "citywide"
+        final = clean_data.loc[~clean_data.citywide.isna()].copy()
+
+    final.drop(columns=["SBA", "PUMA", "CD Name", "SE", "Percent SE"], inplace=True)
+    final.set_index(geography, inplace=True)
+    final.columns = ["units_rentstable_"+ c for c in final.columns]
+    for code, suffix in suffix_mapper.items():
+        final.columns = [c.replace(code, suffix) for c in final.columns]
+
+    if write_to_internal_review:
+        set_internal_review_files(
+            [(final, "units_rentstable.csv", geography)],
+            "housing_security",
+        )  
     return final
 
 def three_mainteinance_units(
     geography: str, write_to_internal_review=False
     ) -> pd.DataFrame:
+    clean_data = load_source_clean_data("units_threemainteinance")
 
-    return final 
-
-def units_affordable(
-    geography: str, write_to_internal_review=False
-    ) -> pd.DataFrame:
-    assert geography in ["citywide", "borough", "puma"]
-
-    clean_df = load_source_clean_data()
     if geography == "puma":
-        final = clean_df.loc[clean_df.Geog.str[0].isna()].copy()
-        final["puma"] = final.Geog.apply(func=clean_PUMAs)
+        final["puma"] = clean_data.PUMA.apply(func=clean_PUMAs)
     elif geography == "borough":
-        clean_df["borough"] = clean_df.Geog.map(borough_name_mapper)
-        final = clean_df.loc[~clean_df.borough.isna()].copy()
+        clean_data["borough"] = clean_data["CD Name"].map(borough_name_mapper)
+        final = clean_data.loc[~clean_data.borough.isna()].copy()
     else:
-        clean_df.loc[clean_df.Geog == "NYC", "citywide"] = "citywide"
-        final = clean_df.loc[~clean_df.citywide.isna()].copy()
+        clean_data.loc[clean_data["CD Name"] == "NYC", "citywide"] = "citywide"
+        final = clean_data.loc[~clean_data.citywide.isna()].copy()
 
-    final.drop(columns=["Geog"], inplace=True)
+    final.drop(columns=["SBA", "PUMA", "CD Name", "SE", "Percent SE"], inplace=True)
     final.set_index(geography, inplace=True)
-    col_order = order_affordable(
-        measures=[i for _, i in suffix_mapper.items()],
-        income=[i for _, i in income_mapper.items()],
-    )
-    final = final.reindex(columns=col_order)
+    final.columns = ["units_threemainteinance_"+ c for c in final.columns]
+    for code, suffix in suffix_mapper.items():
+        final.columns = [c.replace(code, suffix) for c in final.columns]
 
     if write_to_internal_review:
         set_internal_review_files(
-            [(final, "units_affordable.csv", geography)],
+            [(final, "units_threemainteinance.csv", geography)],
             "housing_security",
-        )
-    return final
+        )  
+    return final 
 
 def load_source_clean_data(indicator: str) -> pd.DataFrame:
-    if indicator == "":
+    if indicator == "units_rentstable":
         usecols = [x for x in range(10)]
     else:
         usecols = [x for x in range(2)] + [x for x in range(11, 15)]
@@ -85,18 +79,10 @@ def load_source_clean_data(indicator: str) -> pd.DataFrame:
         #'sheet_name': "AffordableAMI",
         "filepath_or_buffer": "resources/housing_security/2017_HVS_EDDT.csv",
         'usecols': usecols,
-        "header": 0,
+        "header": 1,
         "nrows": 63
     }
-
     df = pd.read_csv(**read_csv_arg)
-
-    cols = df.columns
-
-    ncols = [c.replace(".1", "") for c in cols]
-
-    ncols = [indicator + "_" + c for c in ncols]
-
-    df.columns = ncols      
+    df.columns = [c.replace(".1", "") for c in df.columns]
 
     return df

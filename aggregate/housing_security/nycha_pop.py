2887ddb364b32c643779f6bf5d97d6a638a7ab5b
Te Du <tedudevelop@gmail.com>
2022-04-01 13:05:54 -0400

    set up main accessor function

--- aggregate/housing_security/nycha_pop.py
+++ aggregate/housing_security/nycha_pop.py
@@ -1,7 +1,8 @@
+from aggregate.aggregation_helpers import order_aggregated_columns
 from aggregate.decennial_census.decennial_census_001020 import decennial_census_001020
 import pandas as pd
 from internal_review.set_internal_review_file import set_internal_review_files
-from utils.PUMA_helpers import clean_PUMAs
+from utils.PUMA_helpers import clean_PUMAs, puma_to_borough
 
 race_labels = ["", "_wnh", "_bnh", "_hsp", "_anh", "_oth"]
 
@@ -10,19 +11,23 @@ def nycha_pop(geography: str):
 
     clean_data = load_clean_nycha_data()
     if geography == "puma":
+        final = get_percentage(clean_data)
         final = clean_data
     elif geography == "borough":
-        final = clean_data
+        clean_data["borough"] = puma_to_borough(clean_data.puma)
+        final = get_percentage(clean_data.groupby("borough").agg("sum"))
+        #final = clean_data
     elif geography == "citywide":
-        final = clean_data
+        clean_data["citywide"] = "citywide"
+        final = clean_data.groupby("citywide").agg("sum")
+
+    final = order_aggregated_columns()
 
     return final 
 
 def load_clean_nycha_data():
 
     census20 = decennial_census_001020("puma", year="1519") # this is in fact pulling 2020 census but design has it mapped from 1519 to 2020
-
-    census20_pop = census20.drop(columns=[col for col in census20.columns if "pct" in col])
 
     read_excel_arg = {
         "io": "resources/housing_security/Equitable.Development.Data.Tool.-.Displacement.Risk.Index.2-8-2022.1.xlsx",
@@ -35,16 +40,19 @@ def load_clean_nycha_data():
     nycha_data.rename(columns={"PUMA (2010)": "puma"}, inplace=True)
     nycha_data.puma = nycha_data.puma.apply(func=clean_PUMAs)
     nycha_data.set_index("puma", inplace=True)
-    print(nycha_data)
+    #print(nycha_data)
     # calculating the total for each race categories
     for i in range(6):
         nycha_data[f"nycha_tenants{race_labels[i]}_count"] = nycha_data.iloc[:, i] + nycha_data.iloc[:, i + 6] 
     #print(census20_pop)
-    nycha_pop = pd.concat([census20_pop, nycha_data], axis=1)
+    print(nycha_data.iloc[:, -6:])
+    nycha_pop = pd.concat([census20, nycha_data.iloc[:, -6:]], axis=1)
 
     return nycha_pop
     
 
-def rename_cols(df: pd.DataFrame):
+def get_percentage(df: pd.DataFrame):
+
+
 
     return 

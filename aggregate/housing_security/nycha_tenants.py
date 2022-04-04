2f4a37a98ca280fc0ac1b27bb55ca18f2f665ec5
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-04-04 11:57:35 -0400

    removed unncessary call to decennial census accessor and moved comment to call that is actually used

--- aggregate/housing_security/nycha_tenants.py
+++ aggregate/housing_security/nycha_tenants.py
@@ -3,16 +3,21 @@ from aggregate.aggregation_helpers import order_aggregated_columns
 import pandas as pd
 from internal_review.set_internal_review_file import set_internal_review_files
 from utils.PUMA_helpers import clean_PUMAs, puma_to_borough
+
 dcp_pop_races = ["anh", "bnh", "hsp", "onh", "wnh"]
 
 
 race_labels = ["", "_wnh", "_bnh", "_hsp", "_anh", "_onh"]
 
+
 def nycha_tenants(geography: str, write_to_internal_review=False):
     assert geography in ["citywide", "borough", "puma"]
 
     clean_data = load_clean_nycha_data()
-    census20 = decennial_census_001020(geography=geography, year="1519")
+    census20 = decennial_census_001020(
+        geography=geography, year="1519"
+    )  # this is in fact pulling 2020 census but design has it mapped from 1519 to 2020
+
     if geography == "puma":
         final = get_percentage(pd.concat([census20, clean_data], axis=1))
     elif geography == "borough":
@@ -23,17 +28,21 @@ def nycha_tenants(geography: str, write_to_internal_review=False):
         agg = clean_data.groupby("citywide").agg("sum")
         census20_agg = pd.concat([census20, agg], axis=1)
         final = get_percentage(census20_agg)
-        
+
     final = final.round(2)
 
     order_cols = order_aggregated_columns(
         df=final,
         indicators_denom=[("pop",), ("nycha_tenants",)],
-        categories={"pop": ["pop"], "nycha_tenants": ["nycha_tenants"], "race": dcp_pop_races},
+        categories={
+            "pop": ["pop"],
+            "nycha_tenants": ["nycha_tenants"],
+            "race": dcp_pop_races,
+        },
         return_col_order=True,
-        exclude_denom=True
+        exclude_denom=True,
     )
-    # 24 final columns match the results 
+    # 24 final columns match the results
     final_cols = [col for col in order_cols if "cv" not in col]
     final_cols = [col for col in final_cols if "moe" not in col]
     final = final.reindex(columns=final_cols)
@@ -44,18 +53,17 @@ def nycha_tenants(geography: str, write_to_internal_review=False):
             "housing_security",
         )
 
-    return final 
+    return final
+
 
 def load_clean_nycha_data():
-
-    census20 = decennial_census_001020("puma", year="1519") # this is in fact pulling 2020 census but design has it mapped from 1519 to 2020
 
     read_excel_arg = {
         "io": "resources/housing_security/Equitable.Development.Data.Tool.-.Displacement.Risk.Index.2-8-2022.1.xlsx",
         "sheet_name": "PUMA",
         "usecols": "A, F:Q",
         "nrows": 41,
-        "dtype": float
+        "dtype": float,
     }
     nycha_data = pd.read_excel(**read_excel_arg)
     nycha_data.rename(columns={"PUMA (2010)": "puma"}, inplace=True)
@@ -65,10 +73,12 @@ def load_clean_nycha_data():
     nycha_data.set_index("puma", inplace=True)
     # calculating the total for each race categories
     for i in range(6):
-        nycha_data[f"nycha_tenants{race_labels[i]}_count"] = nycha_data.iloc[:, i] + nycha_data.iloc[:, i + 6] 
+        nycha_data[f"nycha_tenants{race_labels[i]}_count"] = (
+            nycha_data.iloc[:, i] + nycha_data.iloc[:, i + 6]
+        )
 
     return nycha_data.iloc[:, -8:]
-    
+
 
 def get_percentage(df: pd.DataFrame):
 
@@ -76,6 +86,8 @@ def get_percentage(df: pd.DataFrame):
         if r == "":
             df["nycha_tenants_pct"] = df["nycha_tenants_count"] / df["pop_count"] * 100
         else:
-            df[f"nycha_tenants{r}_pct"] = df[f"nycha_tenants{r}_count"] / df[f"pop{r}_count"] * 100
+            df[f"nycha_tenants{r}_pct"] = (
+                df[f"nycha_tenants{r}_count"] / df[f"pop{r}_count"] * 100
+            )
 
     return df

1b1082d623e0db64bae91e88d2bf2c9989684e65
Te Du <tedudevelop@gmail.com>
2022-08-17 13:10:51 -0400

    remove version from config

--- ingest/ingestion_helpers.py
+++ ingest/ingestion_helpers.py
@@ -31,25 +31,7 @@ def get_dataset_version(name: str) -> str:
 def read_from_S3(name: str, cols: list) -> pd.DataFrame:
     read_version = get_dataset_version(name)
     df = pd.read_csv(
-        f"{BASE_URL}/{name}/{read_version}/{name}.csv", dtype=str, index_col=False, usecols=cols
+        f".library/{name}/{read_version}/{name}.csv", dtype=str, index_col=False, usecols=cols
     )
-    version_from_config(name, read_version)
+    add_version(dataset=name, version=read_version)
     return df
-
-
-def version_from_config(name, read_version):
-    client = boto3.client(
-        "s3",
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        endpoint_url=os.environ["AWS_S3_ENDPOINT"],
-    )
-    obj = client.get_object(
-        Bucket="edm-recipes", Key=f"datasets/{name}/{read_version}/config.json"
-    )
-    file_content = str(obj["Body"].read(), "utf-8")
-    json_content = json.loads(file_content)
-    version = json_content["dataset"]["version"]
-    if version.isnumeric():
-        version = int(version)
-    add_version(dataset=name, version=version)

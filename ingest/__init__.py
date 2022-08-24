6c52d67083a4c2a12c1ea876f41894588d2b54b4
Te Du <tedudevelop@gmail.com>
2022-08-24 13:21:08 -0400

    remove extra lines

--- ingest/__init__.py
+++ ingest/__init__.py
@@ -6,9 +6,3 @@ load_dotenv()
 
 BASE_URL = "https://nyc3.digitaloceanspaces.com/edm-recipes/datasets"
 BASE_PATH = Path(__file__).parent / ".cache"
-
-# if not os.path.isdir(BASE_PATH):
-#     os.makedirs(BASE_PATH, exist_ok=True)
-#     # create .gitignore so that files in this directory aren't tracked
-#     with open(f"{BASE_PATH}/.gitignore", "w") as f:
-#         f.write("*")

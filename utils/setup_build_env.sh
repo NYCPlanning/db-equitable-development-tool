8f41020ce5a567c44d8fe0cb63691181fc5eb075
Damon McCullough <dmccullough@planning.nyc.gov>
2023-03-09 15:25:59 -0500

    install python requirements in dev container

---
+++ utils/setup_build_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Sets up enviroment for running build scripts in either local devcontainer or github action.
+set -e
+
+apt-get update
+
+# Install R
+apt-get -y install r-base
+
+# Install python packages
+python3 -m pip install --upgrade pip
+python3 -m pip install --requirement requirements.txt

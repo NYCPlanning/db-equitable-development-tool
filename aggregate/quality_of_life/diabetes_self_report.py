783771cc984a58f59f44f1207fc26f4fcd93ffdf
Damon McCullough <dmccullough@planning.nyc.gov>
2023-03-27 17:20:45 -0400

    ingest new transportation data for 2023 update (#319)
    * add test for ingestion helpers
    
    * add ingest_helpers.py
    
    * add ingest files to test triggers
    
    * latest health excel file
    
    * don't need R in dev container
    
    * use read_excel helper in health script
    
    * fix file
    
    * change name of dict with column mappings
    
    * drop todo note
    
    * use local file instead of s3
    
    * process new transportation data
    
    * add transportation data
    
    * less printing
    
    * better names for tests
    
    * write metadata when reading from excel

--- aggregate/quality_of_life/diabetes_self_report.py
+++ aggregate/quality_of_life/diabetes_self_report.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-# from aggregate.quality_of_life.self_reported_health import load_clean_source_data
+from ingest.ingestion_helpers import read_from_excel
 from utils.CD_helpers import community_district_to_PUMA, borough_name_mapper
 from internal_review.set_internal_review_file import set_internal_review_files
 
 SOURCE_DATA_FILE = "resources/quality_of_life/diabetes_self_report/diabetes_self_report_processed_2023.xlsx"
-
-SOURCE_DATA_COLUMNS = {
+CATEGORY = "quality_of_life"
+SOURCE_SHEET_NAME = "DCHP_Diabetes_SelfRepHealth"
+SOURCE_INDICATOR_COLUMNS = {
+    "diabetes": "A:C, J:M",
+    "self_reported": "A:G",
+}
+COLUMN_MAPPINGS = {
     "cd_number": "ID",
     "percent_diabetes": "Diabetes",
     "percent_self_report": "Self_Rep_Health",
@@ -17,18 +22,14 @@ SOURCE_DATA_COLUMNS = {
 
 def health_diabetes(geography: str, write_to_internal_review=False):
     clean_df = load_clean_source_data(geography, "diabetes")
-    clean_df["pct"] = clean_df[SOURCE_DATA_COLUMNS["percent_diabetes"]]
+    clean_df["pct"] = clean_df[COLUMN_MAPPINGS["percent_diabetes"]]
 
     clean_df["lower_pct_moe"] = (
-        clean_df[
-            f'{SOURCE_DATA_COLUMNS["percent_diabetes"]}_{SOURCE_DATA_COLUMNS["ci_lower"]}'
-        ]
+        clean_df[f'{COLUMN_MAPPINGS["percent_diabetes"]}_{COLUMN_MAPPINGS["ci_lower"]}']
         - clean_df["pct"]
     )
     clean_df["upper_pct_moe"] = (
-        clean_df[
-            f'{SOURCE_DATA_COLUMNS["percent_diabetes"]}_{SOURCE_DATA_COLUMNS["ci_upper"]}'
-        ]
+        clean_df[f'{COLUMN_MAPPINGS["percent_diabetes"]}_{COLUMN_MAPPINGS["ci_upper"]}']
         - clean_df["pct"]
     )
 
@@ -45,17 +46,17 @@ def health_diabetes(geography: str, write_to_internal_review=False):
 
 def health_self_reported(geography: str, write_to_internal_review=False):
     clean_df = load_clean_source_data(geography, "self_reported")
-    clean_df["pct"] = clean_df[SOURCE_DATA_COLUMNS["percent_self_report"]]
+    clean_df["pct"] = clean_df[COLUMN_MAPPINGS["percent_self_report"]]
 
     clean_df["lower_pct_moe"] = (
         clean_df[
-            f'{SOURCE_DATA_COLUMNS["percent_self_report"]}_{SOURCE_DATA_COLUMNS["ci_lower"]}'
+            f'{COLUMN_MAPPINGS["percent_self_report"]}_{COLUMN_MAPPINGS["ci_lower"]}'
         ]
         - clean_df["pct"]
     )
     clean_df["upper_pct_moe"] = (
         clean_df[
-            f'{SOURCE_DATA_COLUMNS["percent_self_report"]}_{SOURCE_DATA_COLUMNS["ci_upper"]}'
+            f'{COLUMN_MAPPINGS["percent_self_report"]}_{COLUMN_MAPPINGS["ci_upper"]}'
         ]
         - clean_df["pct"]
     )
@@ -74,43 +75,25 @@ def health_self_reported(geography: str, write_to_internal_review=False):
 def load_clean_source_data(geography: str, indicator: str):
     assert geography in ["citywide", "borough", "puma"]
 
-    indicator_columns = {
-        "diabetes": "A:C, J:M",
-        "self_reported": "A:G",
-    }
-
-    read_excel_arg = {
-        "io": SOURCE_DATA_FILE,
-        "sheet_name": "DCHP_Diabetes_SelfRepHealth",
-        "usecols": indicator_columns[indicator],
-    }
-
-    df = pd.read_excel(**read_excel_arg)
-    print(f"Shape of {indicator} data is {df.shape}")
-    print(f"Columns in {indicator} data {df.columns}")
+    df = read_from_excel(file_path=SOURCE_DATA_FILE, category=CATEGORY, sheet_name=SOURCE_SHEET_NAME)
 
     if geography == "puma":
         boro = {"2": "BX", "3": "BK", "1": "MN", "4": "QN", "5": "SI"}
 
         df = df[df["Borough"].isin(list(borough_name_mapper.keys()))]
-        print(f"Shape of {indicator} {geography} data is {df.shape}")
 
-        df["CD Code"] = df[SOURCE_DATA_COLUMNS["cd_number"]].astype(str).str[0].map(
+        df["CD Code"] = df[COLUMN_MAPPINGS["cd_number"]].astype(str).str[0].map(
             boro
-        ) + df[SOURCE_DATA_COLUMNS["cd_number"]].astype(str).str[-2:].astype(
-            int
-        ).astype(
+        ) + df[COLUMN_MAPPINGS["cd_number"]].astype(str).str[-2:].astype(int).astype(
             str
         )
         df = community_district_to_PUMA(df, CD_col="CD Code")
         df.drop_duplicates(subset=["puma"], keep="first", inplace=True)
     elif geography == "borough":
         df = df[df["Borough"] == "NYC"]
-        print(f"Shape of {indicator} {geography} data is {df.shape}")
         df["borough"] = df["Borough"].str.strip().map(borough_name_mapper)
     else:
         df = df[df["Borough"] == "City"]
-        print(f"Shape of {indicator} {geography} data is {df.shape}")
         df["citywide"] = "citywide"
 
     clean_df = df.set_index(geography)

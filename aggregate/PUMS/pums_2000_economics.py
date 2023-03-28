6423fd55af3e10bbeea788a92e56c53b37ffbc69
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-03-28 16:19:56 -0400

    revert 2000 pums scripts for simplicity

--- aggregate/PUMS/pums_2000_economics.py
+++ aggregate/PUMS/pums_2000_economics.py
@@ -47,9 +47,11 @@ def rename_cols(df):
     return df
 
 
-def pums_2000_economics(geography: str, write_to_internal_review=False):
-    """Main accessor"""
+def pums_2000_economics(geography: str, year="2000", write_to_internal_review=False):
+    """Main accessor. I know passing year here is silly, need to write it his way to
+    export. Needs refactor"""
     assert geography in ["puma", "borough", "citywide"]
+    assert year == "2000"
 
     df = load_2000_census_pums_all_data()
 

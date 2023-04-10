50152ead18b2fac56d1f66f5cb6df91720d06b84
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-04-10 16:05:58 +0000

    override branch name in github workflows

--- external_review/export_DO.sh
+++ external_review/export_DO.sh
@@ -2,23 +2,28 @@
 
 function export_all {
     DATE=$(date "+%Y-%m-%d")
-    branchname=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
     SPACES="spaces/edm-publishing/db-eddt/${branchname}"
+    echo $SPACES
     mc cp -r .staging/* $SPACES/$DATE/
     mc cp -r .staging/* $SPACES/latest/
 }
 
 function export_category {
     DATE=$(date "+%Y-%m-%d")
-    branchname=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
     SPACES="spaces/edm-publishing/db-eddt/${branchname}"
     mc cp -r .staging/$1/* $SPACES/$DATE/$1/
     mc cp -r .staging/$1/* $SPACES/latest/$1/
 }
 
-if [ $# -eq 0 ] || [ $1 == 'all' ]
-    then
-        export_all
+# a little junky, but only leaving here as option when exporting all as this is only used in github workflow
+if [ $1 == '--github_ref' ]; then
+    branchname=$2
+else 
+    branchname=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+fi
+
+if [ $# -eq 0 ] || [ $1 == 'all' ]; then
+    export_all
 else
     export_category $1
 fi

3582ff05a42eca0333ef72fac8e3705fc60a6cd0
Finn van Krieken <fvankrieken@planning.nyc.gov>
2023-04-17 21:13:51 +0000

    update dhs shelter years

--- ingest/housing_security/DHS_shelter.py
+++ ingest/housing_security/DHS_shelter.py
@@ -7,8 +7,6 @@ import requests
 
 
 def load_DHS_shelter(year: int) -> pd.DataFrame:
-    assert year in [2020, 2022]
-
     url = "https://data.cityofnewyork.us/resource/ur7y-ziyb.json"
     timestamp = datetime(year, 3, 31).isoformat()
     query_str = f"?report_date={timestamp}"

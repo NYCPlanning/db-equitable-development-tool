62d6acb29d543579fc7c4eb6d2103f7e0806ff76
fvankrieken <fvankrieken@planning.nyc.gov>
2023-05-05 08:12:34 -0700

    Fvk 2023 q2 update mortality (#326)
    * fix devcontainer build and test workflow to have mc
    
    * updated source from WS

--- utils/setup_build_env.sh
+++ utils/setup_build_env.sh
@@ -14,3 +14,10 @@ python3 -m pip install --requirement requirements.txt
 
 # Install required R package
 Rscript -e "install.packages('survey')"
+
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+mv ./mc /usr/bin
+mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
